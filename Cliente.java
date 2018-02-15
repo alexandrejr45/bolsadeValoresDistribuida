9c0509982ea9910dcb0e5b58bd0f3a748ed62b75
Alexandre Silva <alexandresilva933@gmail.com>
2018-02-15 10:10:12 -0300

    first

---
+++ Cliente.java
@@ -0,0 +1,16 @@
+public class Cliente {
+
+    private String nome;
+    private int idade;
+
+
+    public setNome(Sring nome){
+        this.nome = nome;
+    }
+
+    public getNome(){
+        return this.nome;
+    }
+
+
+}
