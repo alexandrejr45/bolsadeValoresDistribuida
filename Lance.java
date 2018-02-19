6e0f4f3bdfeb108aa6b2ce2c23b407f25f077a93
Alexandre Silva <alexandresilva933@gmail.com>
2018-02-19 00:26:57 -0300

    commitado

---
+++ Lance.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+import java.rmi.*;
+
+public class Lance implements Serializable{
+    private Double valor;
+
+    public Lance(Usuario usuario, Double valor){}
+
+    
+
+}
