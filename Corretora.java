009e1330a2480dbf8cf3012b3ea32b356aeea03e
Alexandre Silva <alexandresilva933@gmail.com>
2018-02-20 23:21:17 -0300

    atualizacoes

--- Corretora.java
+++ Corretora.java
@@ -1,87 +1,87 @@
-import java.io.*;
-import java.util.*;
-import java.rmi.*;
+  import java.io.*;
+  import java.util.*;
+  import java.rmi.*;
 
 
-public class Corretora implements Serializable{
-    private static Corretora corretora;
-    private Corretora(){};
+  public class Corretora implements Serializable{
+      private static Corretora corretora;
+      private Corretora(){};
 
-    private static ArrayList<Usuario> lista = new ArrayList<Usuario>();
-    private static ArrayList<Lance> lances = new ArrayList<Lance>();
+      private static ArrayList<Usuario> lista = new ArrayList<Usuario>();
+      private static ArrayList<Lance> lances = new ArrayList<Lance>();
 
-    public static Corretora getInstancia(){
-      if (Corretora.corretora == null) {
-            Corretora.corretora = new Corretora();
-        }
-        return corretora;
-    }
-
-    public static boolean adicionarCliente(Usuario cliente){
-        if(cliente != null){
-          lista.add(cliente);
-        }
-
-        return true;
-    }
-
-    public ArrayList retornaLista(){
-      return this.lista;
-    }
-
-    public void mostraClientes(ArrayList clientes){
-      Usuario p;
-      System.out.println ("Numero de clientes cadastrados: " + clientes.size());
-      for (int i=0; i < clientes.size(); i++) {
-        p = (Usuario) clientes.get(i);
-        System.out.println ("--------------------------------------------");
-        System.out.println ("Loops = ");
-        System.out.println (p.getNome());
-
+      public static Corretora getInstancia(){
+        if (Corretora.corretora == null) {
+              Corretora.corretora = new Corretora();
+          }
+          return corretora;
       }
-    }
 
-    public void mostraLances(ArrayList lances){
-      Lance p;
-      System.out.println ("Numero de lances cadastrados: " + lances.size());
-      for (int i=0; i < lances.size(); i++) {
-        p = (Lance) lances.get(i);
-        System.out.println ("--------------------------------------------");
-        System.out.println ("Lance do Usuario = " + p.getUsuario().getNome());
-        System.out.println ("Valor do Lance = " + p.getValor());
+      public static boolean adicionarCliente(Usuario cliente){
+          if(cliente != null){
+            lista.add(cliente);
+          }
 
+          return true;
       }
-    }
 
-    public static boolean adicionarLance(Lance lance){
-      if(lance != null){
-        lances.add(lance);
-        return true;
-      }else{
-        return false;
+      public ArrayList retornaLista(){
+        return this.lista;
       }
-    }
 
-    public ArrayList<Lance> retornaLances(){
-      return this.lances;
-    }
+      public void mostraClientes(ArrayList clientes){
+        Usuario p;
+        System.out.println ("Numero de clientes cadastrados: " + clientes.size());
+        for (int i=0; i < clientes.size(); i++) {
+          p = (Usuario) clientes.get(i);
+          System.out.println ("--------------------------------------------");
+          System.out.println ("Loops = ");
+          System.out.println (p.getNome());
 
-    public ArrayList<Lance> retornaLancesSaldo(Usuario usuario){
-      ArrayList<Lance> listaFinal = new ArrayList<Lance>();
-      ArrayList<Lance> lances = retornaLances();
-      Lance p;
-
-
-      for(int i = 0; i < lances.size(); i++){
-         p = (Lance) lances.get(i);
-        if(p.getValor() <= usuario.getSaldo()){
-          listaFinal.add(p);
         }
       }
 
-      return listaFinal;
+      public void mostraLances(ArrayList lances){
+        Lance p;
+        System.out.println ("Numero de lances cadastrados: " + lances.size());
+        for (int i=0; i < lances.size(); i++) {
+          p = (Lance) lances.get(i);
+          System.out.println ("--------------------------------------------");
+          System.out.println ("Lance do Usuario = " + p.getUsuario().getNome());
+          System.out.println ("Valor do Lance = " + p.getValor());
 
-    }
+        }
+      }
 
-  
-}
+      public static boolean adicionarLance(Lance lance){
+        if(lance != null){
+          lances.add(lance);
+          return true;
+        }else{
+          return false;
+        }
+      }
+
+      public ArrayList<Lance> retornaLances(){
+        return this.lances;
+      }
+
+      public ArrayList<Lance> retornaLancesSaldo(Usuario usuario){
+        ArrayList<Lance> listaFinal = new ArrayList<Lance>();
+        ArrayList<Lance> lances = retornaLances();
+        Lance p;
+
+
+        for(int i = 0; i < lances.size(); i++){
+          p = (Lance) lances.get(i);
+          if(p.getValor() <= usuario.getSaldo()){
+            listaFinal.add(p);
+          }
+        }
+
+        return listaFinal;
+
+      }
+
+    
+  }
--- Servidor.java
+++ Servidor.java
@@ -1,100 +1,100 @@
-import java.lang.*;
-import java.io.*;
-import java.util.*;
-import java.lang.String;
-import javax.swing.*;
-import java.rmi.*;
-import java.rmi.server.*;
+  import java.lang.*;
+  import java.io.*;
+  import java.util.*;
+  import java.lang.String;
+  import javax.swing.*;
+  import java.rmi.*;
+  import java.rmi.server.*;
 
-public class Servidor extends UnicastRemoteObject implements Interface{
-  public Servidor() throws RemoteException { super(); };
+  public class Servidor extends UnicastRemoteObject implements Interface{
+    public Servidor() throws RemoteException { super(); };
 
-  private Corretora corretora = Corretora.getInstancia();
+    private Corretora corretora = Corretora.getInstancia();
 
-  public boolean registraCliente(Usuario cliente) throws RemoteException{
-    System.out.println("Servidor recebeu uma chamada para registrar um Cliente");
-
-    try{
-
-      if(corretora.adicionarCliente(cliente)){
-        System.out.println("Cliente cadastrado");
-
-      }
-
-      return true;
-    }catch (Exception e) {
-      e.printStackTrace();
-      return false;
-    }
-  }
-
-  public ArrayList retornaClientes() throws RemoteException{
-    System.out.println("Servidor recebeu uma chamada para listar os clientes");
-
-    try{
-      return corretora.retornaLista();
-
-    }catch(Exception e){
-      e.printStackTrace();
-      return null;
-    }
-
-  }
-
-  public boolean registrarLance(Lance lance) throws RemoteException{
-    System.out.println("Servidor recebeu uma chamada para registrar um Lance");
-
-    try{
-
-      if(corretora.adicionarLance(lance)){
-        System.out.println("Lance cadastrado");
-        return true;
-      }else{
-        return false;
-      }
-    }catch (Exception e) {
-      e.printStackTrace();
-      return false;
-    }
-  }
-
-  public ArrayList<Lance> retornaLancesUsuario(Usuario user) throws RemoteException{
-      System.out.println("Servidor recebeu uma chamada para listar os lances ");
+    public boolean registraCliente(Usuario cliente) throws RemoteException{
+      System.out.println("Servidor recebeu uma chamada para registrar um Cliente");
 
       try{
-          return corretora.retornaLancesSaldo(user);
 
+        if(corretora.adicionarCliente(cliente)){
+          System.out.println("Cliente cadastrado");
+
+        }
+
+        return true;
       }catch (Exception e) {
+        e.printStackTrace();
+        return false;
+      }
+    }
+
+    public ArrayList retornaClientes() throws RemoteException{
+      System.out.println("Servidor recebeu uma chamada para listar os clientes");
+
+      try{
+        return corretora.retornaLista();
+
+      }catch(Exception e){
         e.printStackTrace();
         return null;
       }
-  }
 
-  public static void main(String args[]) {
-
-    try{
-      System.out.println("Criando o objeto servidor servidor...");
-      Servidor servidor = new Servidor(); // cria um objeto
-      System.out.println("Conectando o objeto cadastro no Registry...");
-      Naming.rebind("rmi:///cadastro", servidor); // registra o objeto forn como "produto"
-
-    } catch (Exception e) {
-      System.out.println("Servidor.main: " + e);
     }
 
-    System.out.println("Pronto para receber chamadas RMI...");
+    public boolean registrarLance(Lance lance) throws RemoteException{
+      System.out.println("Servidor recebeu uma chamada para registrar um Lance");
 
-    try{
-      System.out.println("Criando o objeto servidor servidor...");
-      Servidor servidor = new Servidor(); // cria um objeto
-      System.out.println("Conectando o objeto cadastro no Registry...");
-      Naming.rebind("rmi:///cadastro", servidor); // registra o objeto forn como "produto"
+      try{
 
-    } catch (Exception e) {
-      System.out.println("Servidor.main: " + e);
+        if(corretora.adicionarLance(lance)){
+          System.out.println("Lance cadastrado");
+          return true;
+        }else{
+          return false;
+        }
+      }catch (Exception e) {
+        e.printStackTrace();
+        return false;
+      }
     }
 
-    System.out.println("Pronto para receber chamadas RMI...");
+    public ArrayList<Lance> retornaLancesUsuario(Usuario user) throws RemoteException{
+        System.out.println("Servidor recebeu uma chamada para listar os lances ");
 
+        try{
+            return corretora.retornaLancesSaldo(user);
+
+        }catch (Exception e) {
+          e.printStackTrace();
+          return null;
+        }
+    }
+
+    public static void main(String args[]) {
+
+      try{
+        System.out.println("Criando o objeto servidor servidor...");
+        Servidor servidor = new Servidor(); // cria um objeto
+        System.out.println("Conectando o objeto cadastro no Registry...");
+        Naming.rebind("rmi:///cadastro", servidor); // registra o objeto forn como "produto"
+
+      } catch (Exception e) {
+        System.out.println("Servidor.main: " + e);
+      }
+
+      System.out.println("Pronto para receber chamadas RMI...");
+
+      try{
+        System.out.println("Criando o objeto servidor servidor...");
+        Servidor servidor = new Servidor(); // cria um objeto
+        System.out.println("Conectando o objeto cadastro no Registry...");
+        Naming.rebind("rmi:///cadastro", servidor); // registra o objeto forn como "produto"
+
+      } catch (Exception e) {
+        System.out.println("Servidor.main: " + e);
+      }
+
+      System.out.println("Pronto para receber chamadas RMI...");
+
+    }
   }
-}
